a818e69c2f1089d3b17b2c5ac41c4a555f0782bb
pyropecs <praveengopu0105@gmail.com>
2024-09-15 17:56:49 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import tasks.*;
+
+public class Main {
+public static void main(String[] args){
+int[] nums = {2,3,4,5};
+    Copy c1 = new Copy(2,nums);
+Copy c2 = c1; //shallow copy only copying reference of the object
+System.out.println(c2.equals(c1)); // true because object variable referencing to same object
+Copy c3 = new Copy(c1); // deep copy using copy constructor
+    System.out.println(c3.equals(c1));//false because newly object is created and values are copied into it
+
+
+}
+
+}
---
+++ src/tasks/Copy.java
@@ -0,0 +1,22 @@
+package tasks;
+
+public class Copy {
+private int num;
+private int[] arr;
+public Copy(int num,int[] arr){
+    this.num = num;
+    this.arr = arr;
+}
+
+public void display(){
+    System.out.println(arr.toString());
+}
+
+
+public Copy(Copy c){
+//deep copy
+this.num = c.num;
+this.arr= c.arr;
+}
+
+}
